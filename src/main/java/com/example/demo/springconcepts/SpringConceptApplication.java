194f88755c0313502daf5fec083cea15194bda6a
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-03-01 23:11:21 +0530

    Using EmployeeBean DepartmentBean, Demonstrate dependency injection

--- src/main/java/com/example/demo/springconcepts/SpringConceptApplication.java
+++ src/main/java/com/example/demo/springconcepts/SpringConceptApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo.springconcepts;
 
 import com.example.demo.springconcepts.component.DemoBean;
+import com.example.demo.springconcepts.component.EmployeeBean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,21 +10,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com.example.demo.springconcepts")
-public class SpringConceptApplication {
+public class SpringConceptApplication {  //curl -v "http://localhost:8000/hello/query?name=Mark"
+    // link - http://localhost:8000/hello/query?name=Mark
 
     public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 
     public static void main(String[] args) {
-        // Start Spring application
+        logger.debug("Welcome to Spring Concepts Demo");
         ApplicationContext context = SpringApplication.run(SpringConceptApplication.class, args);
 
         // Log messages
-        logger.debug("Welcome to Spring Concept Demo");
-
+        logger.debug("Checking Context : {}",context.getBean(DemoBean.class));
+        logger.debug("\n***Example Using Autowire annotation on property ***");
         // Ensure DemoBean is found
-        DemoBean demoBean = context.getBean(DemoBean.class);
-        logger.debug("Checking Context: {}", demoBean.sayHello());
 
-        logger.debug("\n*** Example Using @Autowire annotation on property***");
-}
+        EmployeeBean employeeBean=context.getBean(EmployeeBean.class);
+        employeeBean.setEid(104);
+        employeeBean.setEname("Spring Framework Guru");
+        employeeBean.showEmployeeDetails();
+    }
 }
---
+++ src/main/java/com/example/demo/springconcepts/component/DepartmentBean.java
@@ -0,0 +1,23 @@
+package com.example.demo.springconcepts.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class DepartmentBean {
+
+    private String deptName;
+
+    public String getDeptName() {
+
+        return deptName;
+
+    }
+
+    public void setDeptName (String deptName) {
+
+        this.deptName =deptName;
+
+}
+
+}
